01a3c75ffc4a60be92e92d8989d88e3e2987aec0
hanss <hansemusa1@gmail.com>
2021-10-03 12:44:17 -0400

    fixup! upodated second file

---
+++ src/Second.java
@@ -0,0 +1,12 @@
+public class Second {
+    public void m(){
+        System.out.println("first method");
+    }
+
+    public void m(int a){
+        System.out.println("second method");
+    }
+    public void m(String s){
+        System.out.println("third method");
+    }
+}
